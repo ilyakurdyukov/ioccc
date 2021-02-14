a392678ffb5953bc267e45074d7bc35c8f3d8b3c
Ilya Kurdyukov <59548320+ilyakurdyukov@users.noreply.github.com>
2021-02-14 10:41:20 +0700

    2020.12-heart update

--- practice/2020.12-heart/README.md
+++ practice/2020.12-heart/README.md
@@ -2,5 +2,10 @@
 
 Based on this equation:
 
-    (x^2+y^2-1)^3-x^2*y^3<0
+    (x^2+y^2-N^2)^3-x^2*y^3*N<0
 
+Alternate version:
+
+    min(abs(x)+abs(y)-N,x*x+y*y-(abs(x)+y)*N)<0
+
+N is a constant for scaling.
--- practice/2020.12-heart/prog.c
+++ practice/2020.12-heart/prog.c
@@ -1,3 +1,7 @@
+#ifndef IDX
+#define IDX 1
+#endif
+#if IDX==1 // (x^2+y^2-N^2)^3-x^2*y^3*N<0
 
    x,y,z;  main()
  {for(y=15;--y>-11;
@@ -7,4 +11,39 @@
    ?42:32:10))z=x
      *x-400+y*y
         *4;}
+
+#elif IDX==2
+
+   x,y=28  ;main(
+ z){for(;y>-22;y-=2
+ )for(x=25;--x>-25;
+  putchar(x+24?z*z
+  *z<x*x*y*y*y*25?
+    42:32:10))z=
+     -400+x*x+y
+        *y;}
+
+#elif IDX==3 // min(abs(x)+abs(y)-N,x*x+y*y-(abs(x)+y)*N)<0
+
+    x,y,z;   main()
+  {for(y=21;-21<y;y-=
+  2)for(x=21;-21<--x;
+  )z=x<0?-x:x,putchar
+   (x+20?(y<0?z-y<16
+    :z*z+y*y<(z+y)*
+      16)*10-224:
+         10);}
+
+#elif IDX==4
+
+    x,y=21  ;main(
+  z){for(;y>-21;y-=2
+ )for(x=21;x-->-20;)z
+ =x<0?-x:x,putchar(x+
+  20?(y<0?z-y<16:z*z
+    +y*y<(z+y)*16)
+      *10+32:10)
+          ;}
+
+#endif
 
