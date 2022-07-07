7445d48d1d9c0e6e2f59f7c5eaaf08acb017ab90
Ilya Kurdyukov <59548320+ilyakurdyukov@users.noreply.github.com>
2022-07-07 23:34:02 +0700

    2022.07-salvemondo update

--- practice/2022.07-salvemondo/prog.c
+++ practice/2022.07-salvemondo/prog.c
@@ -51,13 +51,31 @@ int main() { puts((char[]){
 #include <stdio.h>
 
 int main() {
+#if 1
+#define PAD 11
 	const char *s1 = "Salve, Mondo!";
 	const char *s2 = "Hello, World!";
+#else // test
+#define PAD 5
+	const char *s1 = "fffffffffff";
+	const char *s2 = "abcdefghijk";
+#endif
 	int i;
 	for (i = 0; s1[i]; i++) {
 		int a = s1[i] - s2[i];
+#if PAD
+		int n = PAD - (a > 0 ? a : -a);
+		int j = 0;
+		if ((n & 1) ^ (a > 0))
+		for (; j < n * 2; j++)
+			putchar("~-"[((n^j)&1)^(j<n)]);
+#endif
 		while (a > 0) printf("~-"), a--;
 		while (a < 0) printf("-~"), a++;
+#if PAD
+		for (; j < n * 2; j++)
+			putchar("~-"[((n^j)&1)^(j<n)]);
+#endif
 		printf("'%c',\n", s1[i]);
 	}
 }
